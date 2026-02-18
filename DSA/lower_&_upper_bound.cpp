42bf5dcfbbcfb9e842377dfa2fc98a1223c09953
Nkosinathi Michael Sibanda <23mh1a05h9@acoe.edu.in>
2026-02-18 15:29:17 +0530

    feat: binary search-lower and upper bound

--- DSA/lower_&_upper_bound.cpp
+++ DSA/lower_&_upper_bound.cpp
@@ -17,14 +17,13 @@ Upper and Lower Bound in Binary Search
     }
 
 cpp(stl)
-*/
 #include <bits/stdc++.h>
 using namespace std;
 int main()
 {
     int n;
     cin >> n;
-    vector<int> arr;
+    vector<int> arr(n);
     for (int i = 0; i < n; i++)
         cin >> arr[i];
     int target;
@@ -35,3 +34,50 @@ int main()
     cout << "Upper bound index: " << upper << endl;
     return 0;
 }
+
+Longest Increasing Subsequence (LIS) using Binary Search
+The longest increasing subsequence (LIS) problem is to find the length of the longest subsequence of a given sequence such that all elements of the subsequence are sorted in increasing order. The
+LIS problem can be solved using dynamic programming with a time complexity of O(n^2), but it can be optimized to O(n log n) using binary search.
+
+// O(n^2) solution using recursion
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(int idx, int prev, vector<int>& nums, int n) {
+    if (idx == n)
+        return 0;
+    int notTake = solve(idx + 1, prev, nums, n);
+    int take = 0;
+    if (nums[idx] > prev)
+        take = 1 + solve(idx + 1, nums[idx], nums, n);
+    return max(take, notTake);
+}
+
+int lengthOfLIS(vector<int>& nums) {
+    int n = nums.size();
+    int ans = solve(0, -1e5, nums, n);
+    return ans;
+}
+
+// O(n log n) solution using binary search
+#include <bits/stdc++.h>
+using namespace std;
+int lengthOfLIS(vector<int>& nums) {
+    vector<int> temp;
+    for (int i = 0; i < nums.size(); i++) {
+        if(i == 0){
+            temp.push_back(nums[i]);
+        }
+        else {
+            if(nums[i] > temp.back()){
+                temp.push_back(nums[i]);
+            }
+            else{
+                int idx = lower_bound(temp.begin(), temp.end(), nums[i]) - temp.begin();
+                temp[idx] = nums[i];
+            }
+        }
+    }
+    return temp.size();
+}
+*/
